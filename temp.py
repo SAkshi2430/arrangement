231d1d93424e45e0ffc01e19ec741fa01d00dd61
SAkshi Kale <66623701+SAkshi2430@users.noreply.github.com>
2021-06-21 23:36:22 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+    
+word = input()
+li=[]
+res=[]
+
+for i in word:
+    if i not in li:
+        li.append(i)
+        
+for i in li:
+    count=0
+    for j in word:
+        if i==j:
+            count+=1
+    else:
+        res.append({"letter":i,"count":count})
+    
+sorted_result = sorted(res, key = lambda i: i['count'], reverse = True)
+
+for dict in sorted_result:
+    print(dict['letter'],'=',dict['count'])
+
+
+
+            
+
+    
+
+
+
+       
+            
