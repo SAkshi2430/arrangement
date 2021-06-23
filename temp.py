edaea7ece674ac7538939ee4b65bb3cc565c7517
SAkshi Kale <66623701+SAkshi2430@users.noreply.github.com>
2021-06-23 20:59:21 +0530

    Add files via upload

--- temp.py
+++ temp.py
@@ -10,22 +10,29 @@ word = input()
 li=[]
 res=[]
 
-for i in word:
-    if i not in li:
-        li.append(i)
-        
-for i in li:
-    count=0
-    for j in word:
-        if i==j:
-            count+=1
-    else:
-        res.append({"letter":i,"count":count})
-    
-sorted_result = sorted(res, key = lambda i: i['count'], reverse = True)
+def most_frequent():
 
-for dict in sorted_result:
-    print(dict['letter'],'=',dict['count'])
+    for i in word:
+        if i not in li:
+            li.append(i)
+            
+    for i in li:
+        count=0
+        for j in word:
+            if i==j:
+                count+=1
+        else:
+            res.append({"letter":i,"count":count})
+        
+    sorted_result = sorted(res, key = lambda i: i['count'], reverse = True)
+    
+    for dict in sorted_result:
+        print(dict['letter'],'=',dict['count'])
+        
+most_frequent()
+    
+    
+
 
 
 
